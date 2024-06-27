e35e83624a2ac91fd0533ddb6f5e2f352649efd0
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-06-28 02:46:52 +0700

    Complete 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+  index = Number(index);
+  
+  if (index < 0) return 'OOPS';
 
+  const fibs = [0, 1];
+  for (let idx = 2; idx <= index; ++idx)
+    fibs[idx] = fibs[idx - 1] + fibs[idx - 2];
+
+  return fibs[index];
 };
 
 // Do not edit below this line
