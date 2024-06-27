a36f3127cdaa3624c412582a5d173762d56c2c34
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-06-28 03:07:35 +0700

    Complete 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const CURRENT_YEAR = 2021;
 
+const getAge = function (person) {
+  if (person.yearOfDeath === undefined)
+    return CURRENT_YEAR - person.yearOfBirth;
+  return person.yearOfDeath - person.yearOfBirth;
+}
+
+const findTheOldest = function (persons) {
+  if (persons.length <= 0) return null;
+
+  let initialPerson = persons[0];
+
+  return persons.reduce((oldest, current) =>
+    (getAge(oldest) > getAge(current) ? oldest : current),
+    initialPerson);
 };
 
 // Do not edit below this line
