fb1ee96787f287d425086fe073ae52adff20d866
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-06-28 01:51:51 +0700

    Complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function (from, to) {
+  if (typeof from !== 'number' || typeof to !== 'number') return 'ERROR';
+  if (from < 0 || to < 0) return 'ERROR';
 
+  if (from > to) [from, to] = [to, from];
+
+  return (from + to) * (to - from + 1) / 2;
 };
 
 // Do not edit below this line
