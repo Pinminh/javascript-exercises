6ba49f51b17926e5cd7e03c430b9c3900274052e
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-06-28 01:59:30 +0700

    Complete 06_leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  if (typeof year !== 'number') return 'ERROR';
 
+  if (year % 400 === 0) return true;
+  else if (year % 100 === 0) return false;
+  else if (year % 4 === 0) return true;
+  
+  return false;
 };
 
 // Do not edit below this line
