826e58a67001ad9469a3c24ae02e640ed542e3bb
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-06-28 02:20:31 +0700

    Complete 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  let sum = 0;
+  for (let num of nums)
+    sum += num;
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+  let prod = 1;
+  for (let num of nums)
+    prod *= num;
+  return prod;
 };
 
-const power = function() {
-	
+const power = function (b, e) {
+  let result = 1;
+  for (let count = 0; count < e; ++count)
+    result *= b;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let fact = 1;
+  if (n === 0) return fact;
+  for (let mul = 1; mul <= n; ++mul)
+    fact *= mul;
+  return fact;
 };
 
 // Do not edit below this line
