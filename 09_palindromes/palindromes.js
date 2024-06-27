10ac9b343a00d0dd98c5fc03d07a2e9caf804fe0
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-06-28 02:33:43 +0700

    Complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (words) {
+  if (typeof words !== 'string') return 'ERROR';
 
+  const punctuations = [',', '.', ' ', '!'];
+  let string = words.split('').filter(c => !punctuations.includes(c)).join('').toLowerCase();
+  let reversed = string.split('').reverse().join('');
+
+  return string == reversed;
 };
 
 // Do not edit below this line
