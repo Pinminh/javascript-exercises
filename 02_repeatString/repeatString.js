8bc54b243fcce9a364f209106353483d86193430
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-06-28 01:15:00 +0700

    Complete 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, times) {
+  if (typeof string !== "string" || typeof times !== "number") return "ERROR";
+  if (times < 0) return "ERROR";
 
+  let result = "";
+
+  for (let count = 0; count < times; ++count)
+    result += string;
+
+  return result;
 };
 
 // Do not edit below this line
