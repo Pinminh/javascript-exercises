460682ca01d60d431b4477b6d4b11c10a2b7001d
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-06-28 01:26:42 +0700

    Complete 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  if (typeof string !== "string") return "ERROR";
 
+  let reversed = "";
+
+  for (let idx = string.length - 1; idx >= 0; --idx) {
+    reversed += string[idx];
+  }
+
+  return reversed;
 };
 
 // Do not edit below this line
