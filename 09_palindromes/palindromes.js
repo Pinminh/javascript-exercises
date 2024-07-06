51e3ecb9af72c3557cc2f20247eef7acecc684ba
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-07-07 01:43:46 +0700

    Shorten 09_palindromes with array methods

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,7 @@
 const palindromes = function (words) {
-  if (typeof words !== 'string') return 'ERROR';
-
-  const punctuations = [',', '.', ' ', '!'];
-  let string = words.split('').filter(c => !punctuations.includes(c)).join('').toLowerCase();
-  let reversed = string.split('').reverse().join('');
-
-  return string == reversed;
+  let isAlphaNum = char => "abcdefghijklmnopqrstuvwxyz0123456789".includes(char);
+  let alphanum = words.toLowerCase().split("").filter(isAlphaNum);
+  return alphanum.join() === alphanum.reverse().join();
 };
 
 // Do not edit below this line
